ce4b43a420070502e0c5df3d337ea31c9c7899dc
Allen Hsu <allenhsu.taiwan@gmail.com>
2022-07-02 20:53:15 +0800

    refactor some things

--- Project/scripts/operate.ts
+++ Project/scripts/operate.ts
@@ -1,9 +1,16 @@
 import deploy from "./deployments";
 import getWalletAccount from "./getWalletAccount";
+import "dotenv/config";
 
 async function main() {
-  const signer = await getWalletAccount();
-  const ballotAddress = await deploy(signer, "Allen", "russ", "Kuro", "IKA");
+  const ownerSigner = await getWalletAccount(process.env.PRIVATE_KEY);
+  const ballotAddress = await deploy(
+    ownerSigner,
+    "Allen",
+    "russ",
+    "Kuro",
+    "IKA"
+  );
 }
 
 main().catch((error) => {
